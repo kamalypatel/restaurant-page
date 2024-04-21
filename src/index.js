bc7752665f4d9826914d2e847e9d089d67b8d5c1
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-04-21 18:06:22 -0400

    made the home page

--- src/index.js
+++ src/index.js
@@ -5,24 +5,56 @@ const content = document.querySelector('#content')
 
 content.classList.add('content')
 
-const navbar = document.createElement('nav')
+const navBar = document.createElement('nav')
 
-navbar.classList.add('navbar')
+navBar.classList.add('navBar')
+
+const logoName = document.createElement('p')
+
+logoName.innerText = "Daniela's"
+logoName.classList.add('logoName')
+
+navBar.appendChild(logoName)
+
+const navButtons = document.createElement('div')
+navButtons.classList.add('navButtons')
 
 for (let i = 1; i <= 3; i++) {
-    const navbutton = document.createElement('button')
+    const navButton = document.createElement('button')
 
-    navbutton.setAttribute('type', 'button')
-    navbutton.classList.add('navbutton')
+    navButton.setAttribute('type', 'button')
+    navButton.classList.add('navButton')
     if (i == 1) {
-        navbutton.innerText = 'Home'
+        navButton.innerText = 'Home'
     } else if (i == 2) {
-        navbutton.innerText = 'Menu'
+        navButton.innerText = 'Menu'
     } else {
-        navbutton.innerText = 'About'
+        navButton.innerText = 'About'
     }
-    navbar.appendChild(navbutton)
+    navButtons.appendChild(navButton)
 }
 
-content.appendChild(navbar)
+navBar.appendChild(navButtons)
+content.appendChild(navBar)
 
+const hero = document.createElement('div')
+
+hero.classList.add('hero')
+
+const heroText = document.createElement('p')
+
+heroText.innerText = 'Welcome to Dining Excellence'
+
+heroText.classList.add('heroText')
+
+hero.appendChild(heroText)
+
+content.appendChild(hero)
+
+const description = document.createElement('p')
+
+description.innerText = 'We are bringing you fine dining in a romantic setting within the Elmwood Village. Sit down with our world class Italian cuisine, enjoy our stellar beverage program, and finish with our 5-Star Gelato.'
+
+description.classList.add('description')
+
+content.appendChild(description)
