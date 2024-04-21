8d2d65211fdd6063e36967b7a6a3d99001449d46
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-04-21 17:33:12 -0400

    working on making a page through javascript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+import './normalize.css'
 import './styles.css'
 
 const content = document.querySelector('#content')
 
 content.classList.add('content')
+
+const navbar = document.createElement('nav')
+
+navbar.classList.add('navbar')
+
+for (let i = 1; i <= 3; i++) {
+    const navbutton = document.createElement('button')
+
+    navbutton.setAttribute('type', 'button')
+    navbutton.classList.add('navbutton')
+    if (i == 1) {
+        navbutton.innerText = 'Home'
+    } else if (i == 2) {
+        navbutton.innerText = 'Menu'
+    } else {
+        navbutton.innerText = 'About'
+    }
+    navbar.appendChild(navbutton)
+}
+
+content.appendChild(navbar)
+
